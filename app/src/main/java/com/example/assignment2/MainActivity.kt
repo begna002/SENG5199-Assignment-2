228c73b2f8ee445b7e3fde322649bb30f45fd663
motibegna <begna002@umn.edu>
2024-02-17 12:30:42 -0600

    Refactoring

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -37,14 +37,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -244,8 +242,6 @@ class MainActivity : ComponentActivity() {
 
         val imageDetails = "Title: ${response.title}\n" +
                 "Date: ${response.date}"
-
-
 
         Row (
             modifier = Modifier.clickable {
